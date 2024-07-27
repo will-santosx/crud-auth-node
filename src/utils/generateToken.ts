2d9ec9ab55a5a522313b9357ece437edf40209a9
Willians Santos <100812504+will-santosx@users.noreply.github.com>
2024-07-27 17:57:16 -0400

    change jwt validate

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const secret = process.env.JWT_SECRET as string;
 
 export const generateToken = (id: number): string => {
-  return jwt.sign({ id }, secret, { expiresIn: "1h" });
+  return jwt.sign({ id }, secret, { expiresIn: "1d" });
 };
